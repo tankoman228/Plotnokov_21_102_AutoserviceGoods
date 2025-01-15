e2e7b041264e9591c3cd7c5d02482354a00a4694
pr-21.102-ps <tankoman228@gmail.com>
2025-01-15 16:37:02 +0700

    Вход и капча

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Plotnokov_21_102_AutoserviceGoods.Service;
 
 namespace Plotnokov_21_102_AutoserviceGoods
 {
@@ -23,6 +25,112 @@ namespace Plotnokov_21_102_AutoserviceGoods
         public MainWindow()
         {
             InitializeComponent();
+
+            btnEnter.Click += BtnEnter_Click;
+            btnEnterGuest.Click += BtnEnterGuest_Click;
+        }
+
+        private void BtnEnterGuest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Not implemented");
+            throw new NotImplementedException();
+        }
+
+        private void BtnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (!CheckCapctha())
+            {
+                MessageBox.Show("Капча введена неверно!", "Robot detected", MessageBoxButton.OK, MessageBoxImage.Stop);
+                BlockEnter();
+                return;
+            }
+
+            if (ServiceLogin.Login(tbLogin.Text, tbPassword.Password)) {
+                MessageBox.Show("Enter success");
+            }
+            else
+            {
+                if (cap != null)
+                {
+                    BlockEnter();
+                }
+                MessageBox.Show($"Не получилось войти, неверный логин или пароль");
+                OpenCaptcha();
+            }
+        }
+
+        private string cap;
+        private char[] capSymbols = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'a', 'V', 'W', 'L', 'G', 'F', 'D', '1', '2', '3', '4', '5', '6' };
+        private char[] noiseSymbols = { '-', '_', '+', '&', '?', ',', '.', '~', '@', '\a' };
+        private void OpenCaptcha()
+        {
+            captcha.Visibility = Visibility.Visible;
+            Random r = new Random();
+
+            cap = "" + 
+                capSymbols[r.Next(capSymbols.Length)] + 
+                capSymbols[r.Next(capSymbols.Length)] + 
+                capSymbols[r.Next(capSymbols.Length)] + 
+                capSymbols[r.Next(capSymbols.Length)];
+
+            tbCaptcha1.Text = cap[0].ToString();
+            tbCaptcha2.Text = cap[1].ToString();
+            tbCaptcha3.Text = cap[2].ToString();
+            tbCaptcha4.Text = cap[3].ToString();
+
+            tbCaptchaNoise1.Text = "" + 
+                noiseSymbols[r.Next(noiseSymbols.Length)] + 
+                noiseSymbols[r.Next(noiseSymbols.Length)] + 
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)];
+
+            tbCaptchaNoise2.Text = "" +
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)] +
+                noiseSymbols[r.Next(noiseSymbols.Length)];
+
+            tbCaptcha1.FontSize = 90 + (0.5 - r.NextDouble()) * 8;
+            tbCaptcha2.FontSize = 90 + (0.5 - r.NextDouble()) * 8;
+            tbCaptcha3.FontSize = 90 + (0.5 - r.NextDouble()) * 8;
+            tbCaptcha4.FontSize = 90 + (0.5 - r.NextDouble()) * 8;
+        }
+        private bool CheckCapctha()
+        {
+            if (cap == null) return true;
+            if (cap == tbCap.Text) return true;
+
+            OpenCaptcha();
+
+            return false;
+        }
+
+        private void BlockEnter()
+        {
+            btnEnter.IsEnabled = false;
+
+            new Thread(() =>
+            {
+                for (int i = 10; i > 0; i--)
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        btnEnter.Content = "Войти (" + i + ")";
+                    });
+                    Thread.Sleep(1000);
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    btnEnter.IsEnabled = true;
+                    btnEnter.Content = "Войти";
+                });
+
+            }).Start();
         }
     }
 }
---
+++ Service/ServiceLogin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plotnokov_21_102_AutoserviceGoods.DB;
+
+namespace Plotnokov_21_102_AutoserviceGoods.Service
+{
+    internal class ServiceLogin
+    {
+        /// <summary>
+        /// Current user, if null => guest mode
+        /// </summary>
+        public static User CurrentUser { get; set; }
+
+        public static bool Login(string login, string password)
+        {
+            using (var db = new DB.DB())
+            {
+                var usr = db.User.Where(x => x.UserLogin == login).FirstOrDefault();
+                if (usr == null) return false;
+                if (usr.UserPassword != password) return false;
+
+                CurrentUser = usr;
+            }
+
+            return true;
+        }
+    }
+}
